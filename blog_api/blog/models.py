6b55a24aa26754cce1469b139f1ec10bb4d155c3
Christian Amoakohene <kriswale@Christians-MacBook-Pro.local>
2024-11-12 22:59:43 +0000

    views and urls

--- blog_api/blog/models.py
+++ blog_api/blog/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Articles(models.Model):
-    title = models.CharField
+    title = models.CharField(max_length=100, null=True)
+    content = models.TextField(null=True)
+    author = models.CharField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.title
--- blog_api/blog/views.py
+++ blog_api/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import Articles
+from rest_framework.decorators import api_view
+from .serializers import ArticleSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def blog_view(request):
+    if request.method == 'GET':
+        blogs = Articles.objects.all()
+        serializer = ArticleSerializer(blogs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class BlogList(generics.ListCreateAPIView):
+    queryset = Articles.objects.all()
+    serializer_class = ArticleSerializer
+
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+     queryset = Articles.objects.all()
+     serializer_class = ArticleSerializer
